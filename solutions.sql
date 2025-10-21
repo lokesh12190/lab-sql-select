28fd3a869e70d966cbcff6cb0554183013378017
Lokesh Metta <lkshmetta@gmail.com>
2025-10-21 16:10:00 +0200

    second comments

--- solutions.sql
+++ solutions.sql
@@ -14,18 +14,21 @@ JOIN publishers P on P.pub_id = T.pub_id
 group by A.au_id, P.pub_id;
 
 -- Challenge 3---
-SELECT A.au_id, A.au_lname, A.au_fname, count(TA.title_id)AS "Total"
+SELECT A.au_id, A.au_lname, A.au_fname, SUM(T.ytd_sales) AS "TOTAL"
 FROM authors A
-JOIN titleauthor TA on A.au_id = TA.au_id
-group by A.au_id
-order by "Total" DESC  LIMIT 3;
+JOIN titleauthor TA ON A.au_id = TA.au_id
+JOIN titles T ON TA.title_id = T.title_id
+GROUP BY A.au_id
+ORDER BY TOTAL DESC
+LIMIT 3;;
 
 -- Challenge 4---
-SELECT A.au_id, A.au_lname, A.au_fname, count(TA.title_id)AS "Total"
+SELECT A.au_id, A.au_lname, A.au_fname, COALESCE(SUM(T.ytd_sales), 0) AS "TOTAL"
 FROM authors A
-LEFT JOIN titleauthor TA on A.au_id = TA.au_id
-group by A.au_id
-order by "Total" DESC;
+LEFT JOIN titleauthor TA ON A.au_id = TA.au_id
+LEFT JOIN titles T ON TA.title_id = T.title_id
+GROUP BY A.au_id
+ORDER BY TOTAL DESC;
 
 
 
